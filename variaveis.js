63003ebfab4f8d0e1a09f04beeda851178c8a22f
Renato Gabral da Silva <renato.gabral.silva@escola.pr.gov.br>
2024-09-17 12:10:07 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+// tipo number
+
+// var era utilizado para todas as variáveis
+var numero01 = 4
+// let é uma atualização do var e é passível de variação
+let numero02 = 10
+// const é uma maneira de declarar uma variavél que irá permanecer constante
+const numero03 = 7
+
+const soma = numero01 + numero02
+const subtracacao = numero02 - numero01
+const multiplicacao = numero03 * numero02
+const divisao = numero02 / numero01
